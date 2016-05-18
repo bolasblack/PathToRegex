50ca6878b5c804fb08724e18681198b8ff5ee5b2
c4605 <bolasblack@gmail.com>
2016-05-18 09:45:00 +0800

    support other platform

---
+++ PathToRegex-tvOS/PathToRegex-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  PathToRegex-tvOS.h
+//  PathToRegex-tvOS
+//
+//  Created by c4605 on 16/5/18.
+//  Copyright © 2016年 Crossroad Labs, LTD. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for PathToRegex-tvOS.
+FOUNDATION_EXPORT double PathToRegex_tvOSVersionNumber;
+
+//! Project version string for PathToRegex-tvOS.
+FOUNDATION_EXPORT const unsigned char PathToRegex_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PathToRegex_tvOS/PublicHeader.h>
+
+
---
+++ PathToRegexp-watchOS/PathToRegexp-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  PathToRegexp-watchOS.h
+//  PathToRegexp-watchOS
+//
+//  Created by c4605 on 16/5/18.
+//  Copyright © 2016年 Crossroad Labs, LTD. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for PathToRegexp-watchOS.
+FOUNDATION_EXPORT double PathToRegexp_watchOSVersionNumber;
+
+//! Project version string for PathToRegexp-watchOS.
+FOUNDATION_EXPORT const unsigned char PathToRegexp_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PathToRegexp_watchOS/PublicHeader.h>
+
+
